6899dd32913325b6e971bdb73bd28d3d6ef4b125
Yashodeep Deshpande <yashodeepdeshpande84639@gmail.com>
2025-02-18 17:13:12 +0530

    Updated logout functionality

--- auth/logout.php
+++ auth/logout.php
@@ -1,11 +1,18 @@
 <?php
 session_start();
 
+// $_SESSION = [];
+
 // Destroy all sessions
 session_unset();
 session_destroy();
+setcookie(session_name(), "", time() - 3600, "/");
+
+// Prevent browser from caching the page
+// header("Cache-Control: no-cache, must-revalidate");
+// header("Expires: 0");
 
 // Redirect to login page
-header("Location:/Event Management/index.php");
+header("Location:../");
 exit();
 ?>
